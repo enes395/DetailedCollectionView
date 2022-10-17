cc3bd1eeb7ec4f1822d89d8d0db77680e4187596
Muhammed Enes <kiliclimuhammedenes@gmail.com>
2022-10-17 12:30:41 +0300

    Initial Commit

---
+++ DetailedCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetailedCollectionView
+//
+//  Created by Muhammed Enes Kılıçlı on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
